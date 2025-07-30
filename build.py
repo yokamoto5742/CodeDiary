146bce18c2f9221bfe00f9526188ff0050535007
yokamoto5742-h <yokamoto5742@gmail.com>
2025-07-30 13:07:36 +0900

    1st commit

---
+++ build.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+
+from scripts.version_manager import update_version
+
+
+def build_executable():
+    new_version = update_version()
+    subprocess.run([
+        "pyinstaller",
+        "--name=ManualSearch",
+        "--windowed",
+        "--icon=assets/ManualSearch.ico",
+        "--add-data", "utils/config.ini:.",
+        "--add-data", "templates:templates",
+        "main.py"
+    ])
+
+    print(f"Executable built successfully. Version: {new_version}")
+    return new_version
+
+
+if __name__ == "__main__":
+    build_executable()
