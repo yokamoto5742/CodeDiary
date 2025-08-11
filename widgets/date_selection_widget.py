65ca2d651573474cb14eabb6bf24ad911f66fd50
yokamoto5742-h <yokamoto5742@gmail.com>
2025-08-11 15:26:12 +0900

    ✨ feat(widget): 日付選択ウィジェットのコメントと不要な空行を削除

--- widgets/date_selection_widget.py
+++ widgets/date_selection_widget.py
@@ -6,7 +6,6 @@ from tkcalendar import DateEntry
 
 
 class DateSelectionWidget(ttk.LabelFrame):
-    """日付選択ウィジェット"""
     
     def __init__(self, parent, config, **kwargs):
         super().__init__(parent, text="対象期間", padding="5", **kwargs)
@@ -16,11 +15,9 @@ class DateSelectionWidget(ttk.LabelFrame):
         self._setup_ui()
         
     def _setup_ui(self):
-        """UIコンポーネントを設定"""
         self.columnconfigure(1, weight=1)
         self.columnconfigure(3, weight=1)
-        
-        # 開始日ラベル
+
         ttk.Label(self, text="開始日").grid(
             row=0, column=0, sticky=tk.W, padx=(0, 5)
         )
@@ -52,13 +49,11 @@ class DateSelectionWidget(ttk.LabelFrame):
         self.start_date_entry.grid(
             row=0, column=1, sticky=(tk.W, tk.E), padx=(0, 10)
         )
-        
-        # 終了日ラベル
+
         ttk.Label(self, text="終了日").grid(
             row=0, column=2, sticky=tk.W, padx=(0, 5)
         )
-        
-        # 終了日選択
+
         self.end_date_entry = DateEntry(
             self,
             width=12,
@@ -85,15 +80,12 @@ class DateSelectionWidget(ttk.LabelFrame):
         self.end_date_entry.grid(row=0, column=3, sticky=(tk.W, tk.E))
         
     def get_start_date(self):
-        """開始日を取得"""
         return self.start_date_entry.get_date()
         
     def get_end_date(self):
-        """終了日を取得"""
         return self.end_date_entry.get_date()
         
     def validate_dates(self):
-        """日付の妥当性を検証"""
         try:
             start_date = self.get_start_date()
             end_date = self.get_end_date()
